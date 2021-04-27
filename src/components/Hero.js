cc1692c9387182af9c2f66a39ac70f2d1959dc8f
David Yvon <yvon.david@gmail.com>
2021-04-27 18:19:33 +0200

    Add: Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import '../sass/components/hero.scss'
+
+import heroImage from '../images/hero-image.png'
+import arrowIcon from '../images/arrow-icon.svg'
+
+const Hero = () => {
+	return (
+		<section className='hero'>
+			<div className='hero__content'>
+				<h1 className='hero__heading'>
+					<span>City</span> & Concrete
+				</h1>
+				<p className='hero__paragraph'>
+					Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus
+					commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+				</p>
+				<Link className='hero__link' to='#!'>
+					Let's Signup
+					<img className='hero__arrow-icon' src={arrowIcon} alt='' />
+				</Link>
+			</div>
+
+			<div className='hero__image-container'>
+				<img className='hero__image' src={heroImage} alt='City and concrete' />
+			</div>
+		</section>
+	)
+}
+
+export default Hero
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ReactHelmet from '../components/ReactHelmet'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 class App extends React.Component {
 	render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
 			<div className='app'>
 				<ReactHelmet />
 				<Header />
+				<Hero />
 			</div>
 		)
 	}
